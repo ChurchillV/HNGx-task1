f16cf162d3637a763c185fecf313186a67567ef8
ChurchillV <vinchurch70@gmail.com>
2023-09-08 00:43:41 +0000

    Update: index, script

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const currentDay = document.getElementById("day");
-const UTCtime = document.getElementById("time");
+const currentDay = document.querySelector('[data-testId="currentDayOfTheWeek"]');
+const UTCtime = document.querySelector('[data-testId="currentUTCTime"]');
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const today = new Date();
 const hours = today.getUTCHours();
 const minutes = today.getUTCMinutes();
 const seconds = today.getUTCSeconds();
-
-
+// Set current day
 currentDay.innerHTML = daysOfTheWeek[today.getDay()];
-
+// Set current UTC time
 function setTime() {
     UTCtime.innerHTML = hours + ":" + minutes + ":" + seconds ;
 }
 
-setTime()
+setTime();
